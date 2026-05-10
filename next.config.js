6633a6037e63437986277dd287dbae250c758fd5
Rexinsider <132758147+Rexinsider@users.noreply.github.com>
2026-05-10 17:36:07 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['yourprojectid.supabase.co'], // Replace with your Supabase project ID
+  },
+}
+
+module.exports = nextConfig
